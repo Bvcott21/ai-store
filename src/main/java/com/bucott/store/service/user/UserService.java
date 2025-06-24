4f3e8c8c64c1a9dcae05b9b50e6cfaff301e5206
Bvcott21 <56700446+Bvcott21@users.noreply.github.com>
2025-06-25 00:22:33 +0200

    feat: Refactor `UserDetailsService` and implement `UserService` interface
    - Introduced `UserService` interface for additional abstraction and extended implementation in `UserDetailsServiceImpl`.
    - Removed redundant authentication and registration logic from `UserDetailsServiceImpl`.
    - Cleaned imports and eliminated unused dependencies in service classes.

--- src/main/java/com/bucott/store/service/user/UserService.java
+++ src/main/java/com/bucott/store/service/user/UserService.java
@@ -2,12 +2,13 @@ package com.bucott.store.service.user;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-import com.bucott.store.exception.user.EmailNotFoundException;
 import com.bucott.store.exception.user.UserNotFoundException;
 
-public interface UserDetailsService {
+public interface UserService {
+    
     UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
-    UserDetails loadUserByEmail(String email) throws EmailNotFoundException;
+    
+    UserDetails loadUserByEmail(String email) throws UsernameNotFoundException;
+    
     UserDetails loadUserByUsernameOrEmail(String identifier) throws UserNotFoundException;
 }
